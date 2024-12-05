57af19aafed01313e8abb30046310aea65f442ca
unknown <Minskoleopc>
2024-12-05 20:31:01 +0530

    first commit

---
+++ src/App2.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App2(){
+    return <h1>function based component 2</h1>
+}
+
+export default App2
---
+++ src/App3.js
@@ -0,0 +1,9 @@
+import React , {Component} from "react";
+
+class App3 extends Component {
+
+    render(){
+        return <h1>Class based component</h1>
+    }
+}
+export  default App3 
